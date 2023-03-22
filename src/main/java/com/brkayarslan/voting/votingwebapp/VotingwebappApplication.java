2d74a2a2b69e706184449b7f0a80e75a3a2f335a
Berkay Arslan <berkayarslan9@gmail.com>
2023-03-22 13:24:11 +0300

    first-commit

---
+++ src/main/java/com/brkayarslan/voting/votingwebapp/VotingwebappApplication.java
@@ -0,0 +1,14 @@
+package com.brkayarslan.voting.votingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingwebappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VotingwebappApplication.class, args);
+	}
+
+}
